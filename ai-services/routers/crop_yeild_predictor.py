9f76a0289fff208c55be6c4d20c01d2877aac84c
kaushani rasadika <rasadikak@gmail.com>
2026-06-15 19:49:52 +0530

    initilaized crop_yeild_predictor router

--- ai-services/routers/crop_yeild_predictor.py
+++ ai-services/routers/crop_yeild_predictor.py
@@ -1,8 +1,8 @@
-from main import router
+
 from fastapi import APIRouter
 
 router= APIRouter(tags=['crop_yeild_pred'], prefix='/crop_yeild_pred')
 
-@router.post('/crop_yeild_pred')
+@router.post('/')
 def crop_yeild_predictor():
     pass
