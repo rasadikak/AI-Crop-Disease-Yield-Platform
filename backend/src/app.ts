55f670342d2f88b0dc18f41877e2ac4b5ed1b3f1
kaushani rasadika <rasadikak@gmail.com>
2026-06-16 11:24:03 +0530

    created app

---
+++ backend/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+const app= express();
+
+app.use(cors({
+    origin:"https://localhost:5173",  //react dev server
+    credentials: true
+
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/health", (req,res)=>{
+    res.json({status: "ok", message:"AgriSense API running"});
+});
+
+
+export default app;
+    
