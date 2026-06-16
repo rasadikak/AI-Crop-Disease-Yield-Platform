a5f50d856530f6007fea379266e6ecc0633fcb85
kaushani rasadika <rasadikak@gmail.com>
2026-06-16 15:08:27 +0530

    created schemas in index.ts

---
+++ backend/src/types/index.ts
@@ -0,0 +1,24 @@
+export interface Farmer{
+  id: string;
+  name: string;
+  email: string;
+  district: string | null;
+  createdAt: Date;
+}
+
+export interface RegisterBody {
+  name: string;
+  email: string;
+  password: string;
+  district?: string;
+}
+
+export interface LoginBody {
+  email: string;
+  password: string;
+}
+
+export interface AuthResponse{
+    token:string;
+    farmer:Omit<Farmer, "created_at">;
+}
