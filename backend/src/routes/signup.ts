7892fe1300fa9de1f7036d0df6ad3d1bc546c530
kaushani rasadika <rasadikak@gmail.com>
2026-06-16 17:25:23 +0530

    created user register function

--- backend/src/routes/signup.ts
+++ backend/src/routes/signup.ts
@@ -8,4 +8,15 @@ const router= Router();
 router.post("/register", async(req:Request, res:Response)=>{
     const { name, email, password, district } = req.body;
 
+    if(!name || !email || !password || !district){
+        res.status(400).json({error:"Name, email and password are required"});
+        return;
+    }
+
+    if (password.length <6){
+        res.status(400).json({ error: "Password must be at least 6 characters" });
+        return;
+    }
+    
+
 });
