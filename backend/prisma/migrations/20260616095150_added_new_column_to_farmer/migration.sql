d1266704a762517f56ad36ffa8d9cdf5a35b2937
kaushani rasadika <rasadikak@gmail.com>
2026-06-16 15:22:32 +0530

    added new column to farmer table

---
+++ backend/prisma/migrations/20260616095150_added_new_column_to_farmer/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Farmer" ADD COLUMN     "isVerified" BOOLEAN NOT NULL DEFAULT false;
--- backend/src/models/prisma.ts
+++ backend/src/models/prisma.ts
@@ -2,4 +2,17 @@ import {PrismaClient} from "@prisma/client";
 
 const prisma= new PrismaClient();
 
+const connectDatabase= async ()=>{
+    try{
+        await prisma.$connect();
+        console.log("Database connected successfully");
+    }
+    catch(error){
+        console.error("Failed to connect to database:", error);
+        process.exit(1);
+    }
+}
+
+connectDatabase();
+
 export default prisma;
