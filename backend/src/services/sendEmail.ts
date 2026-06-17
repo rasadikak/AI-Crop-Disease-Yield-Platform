03becfe0f2458ecf8d9aa863f81ea72f0e01f67d
kaushani rasadika <rasadikak@gmail.com>
2026-06-17 10:34:54 +0530

    created email sending function

--- backend/src/services/sendEmail.ts
+++ backend/src/services/sendEmail.ts
@@ -1,1 +1,25 @@
 //we use nodemailer
+
+import nodemailer from "nodemailer";
+
+const transport= nodemailer.createTransport({
+      host:   process.env.SMTP_HOST,
+      port:   Number(process.env.SMTP_PORT),
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+  }
+});
+
+type emailType= "verification" | "passwordResetting";
+
+export const sendEmail= async(
+        toEmail:    string,
+        farmerName: string,
+        type:       emailType,
+        token:      string
+
+): Promise<void>=>{
+    const baseURL= process.env.FRONTEND_URL
+
+}
