862ea01378ea7ec94016833a6c8b264685530f11
kaushani rasadika <rasadikak@gmail.com>
2026-06-19 15:00:59 +0530

    home page structure

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,13 @@
+import {Link} from "react-router-dom";
+
 const HomePage=()=>{
     return(
-        <div>AgriSense</div>
+        <div>
+            <h2>AgriSense</h2>
+            <Link to="signup">Sign up</Link>
+            <Link to="login">Sign in</Link>
+        </div>
+
     );
 };
 
