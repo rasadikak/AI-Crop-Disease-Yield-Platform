0d308d331314998d157850a771da57798cdc34e0
kaushani rasadika <rasadikak@gmail.com>
2026-06-19 13:42:07 +0530

    verifyEmailPage.js

--- frontend/src/pages/auth/VerifyEmailPage.tsx
+++ frontend/src/pages/auth/VerifyEmailPage.tsx
@@ -1,7 +1,81 @@
-const VerifyEmailPage=()=>{
+import {useState} from "react";
+import {sendVerificationEmail} from "../../services/authService"
+import { useNavigate, Link,  useSearchParams } from "react-router-dom";
+
+const VerifyEmailPage =()=>{
+
+    
+    const [email, setEmail]= useState("");
+    
+    
+
+    const [isLoading, setIsLoading]= useState(false);
+    const [error, setError]= useState("");
+
+    
+    
+
+    const handleSubmit= async(e:React.FormEvent)=>{
+        e.preventDefault();
+
+        if( !email ){
+            setError("email is required");
+            return;
+        }
+
+        
+        
+
+        
+        setIsLoading(true);
+        setError("");
+
+        try{
+
+            await sendVerificationEmail(email);
+            
+
+        }
+        catch(error:any){
+            setError(error.response?.data?.error || "verification mail sent failed");
+        }finally{
+            setIsLoading(false);
+        }
+
+    }
+
     return(
-        <div>VerifyEmailPage</div>
+        <div>
+            <h2> verify email page</h2>
+
+            
+
+            {error && <p style={{ color: "red" }}>{error}</p>}
+
+            <form onSubmit={handleSubmit}>
+
+                
+
+                <div>
+                    <label>email</label>
+                    <input type="email" value={email} placeholder="" onChange={(e)=>{setEmail(e.target.value)}}/>
+                </div>
+
+                
+
+                
+
+                <button type="submit" disabled={isLoading}>
+                    {isLoading ? "saving" : "save"}
+                </button>
+
+            </form>
+            
+            
+
+        </div>
     );
+    
 };
 
-export default VerifyEmailPage;
+export default VerifyEmailPage ;
