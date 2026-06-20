d3890c4f9797825ca34eaa189a002d70270705e6
kaushani rasadika <rasadikak@gmail.com>
2026-06-20 11:03:43 +0530

    profilpage.tsx

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,7 +1,4 @@
-import {useState,} from "react";
-import { useNavigate, useSearchParams, Link } from "react-router-dom";
-import { login } from "../services/authService";
-import useAuth from "../hooks/useAuth";
+
 
 
 const ProfilePage=()=>{
