5ce364065643f13054ac1af44a40e000ec8b1f42
kaushani rasadika <rasadikak@gmail.com>
2026-06-21 14:44:15 +0530

    app.py

--- ai-services/main.py
+++ ai-services/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from routers import crop_yeild_predictor
+from routers import crop_yeild_predictor, chatbot
 
 app= FastAPI()
 
 
 
 app.include_router(crop_yeild_predictor.router)
+app.include_router(chatbot.router)
