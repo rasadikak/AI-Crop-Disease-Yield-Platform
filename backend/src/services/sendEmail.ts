bba75862a0fb529685466933a8ed2ba2ee59d766
kaushani rasadika <rasadikak@gmail.com>
2026-06-24 17:30:49 +0530

    added googlr smtp service for email sending

--- backend/src/services/sendEmail.ts
+++ backend/src/services/sendEmail.ts
@@ -5,9 +5,10 @@ import nodemailer from "nodemailer";
 const transport= nodemailer.createTransport({
       host:   process.env.SMTP_HOST,
       port:   Number(process.env.SMTP_PORT),
+      secure: false,
       auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD
   }
 });
 
