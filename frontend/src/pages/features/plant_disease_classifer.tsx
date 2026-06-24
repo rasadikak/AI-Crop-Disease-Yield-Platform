2a884a31a6e81040820e29dd3f0aab730e33388a
kaushani rasadika <rasadikak@gmail.com>
2026-06-24 18:15:30 +0530

    updated featurepages

---
+++ frontend/src/pages/features/plant_disease_classifer.tsx
@@ -0,0 +1,11 @@
+const PlantDiseaseClassifierPage=()=>{
+    return (
+        <div>
+            <h2>PlantDiseaseClassifierPage</h2>
+        </div>
+    )
+
+};
+
+export default PlantDiseaseClassifierPage ;
+
