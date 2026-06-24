d7f83e47ac364a4c6d616285ae9c2fe7ccb748d3
kaushani rasadika <rasadikak@gmail.com>
2026-06-24 18:15:55 +0530

    updated featurepages

---
+++ frontend/src/pages/features/weather_anomaly_detector.tsx
@@ -0,0 +1,11 @@
+const WeatherAnomalyDetectorPage=()=>{
+    return (
+        <div>
+            <h2>WeatherAnomalyDetectorPage</h2>
+        </div>
+    )
+
+};
+
+export default WeatherAnomalyDetectorPage ;
+
