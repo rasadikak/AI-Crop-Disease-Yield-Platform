46310cc4156a3948e15f74f7244bc6d95ca45570
kaushani rasadika <rasadikak@gmail.com>
2026-06-24 18:06:18 +0530

    updated dashboard.tsx

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,6 +1,14 @@
+import {Link} from "react-router-dom";
+
 const DashboardPage=()=>{
     return(
-        <div>dahsboard</div>
+        <div>
+            <h2>dashboard</h2>
+            <Link to="/chatbot">chatbot</Link>
+            <Link to="/profile">profile</Link>
+        </div>
+
+
     );
 };
 
