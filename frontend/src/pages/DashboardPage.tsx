1ef33992e18db9d8c57acbd7e08f719f36830999
kaushani rasadika <rasadikak@gmail.com>
2026-06-24 18:17:45 +0530

    updated dashboard

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -6,6 +6,9 @@ const DashboardPage=()=>{
             <h2>dashboard</h2>
             <Link to="/chatbot">chatbot</Link>
             <Link to="/profile">profile</Link>
+            <Link to="/CropYeildPredictor">Crop Yeild Predictor</Link>
+            <Link to="/PlantDiseaseClassifier">Plant Disease Classifier</Link>
+            <Link to="/WeatherAnomalyDetector">Weather Anomaly Detector</Link>
         </div>
 
 
