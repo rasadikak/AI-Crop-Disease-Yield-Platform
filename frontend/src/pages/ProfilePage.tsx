fe6dd905092796c166d5ea6bfc2dffcf4b567e3e
kaushani rasadika <rasadikak@gmail.com>
2026-06-26 11:09:55 +0530

    profilepage.tsx

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,10 +1,28 @@
 
-
+import {Link} from "react-router-dom"
 
 const ProfilePage=()=>{
 
     return (
-        <div>profile page</div>
+        <div>
+        <div>Your profile</div>
+
+        <h3>Mange your account and preferences </h3> 
+
+        your name
+        <input type="text" name="name" value={}></input>
+        <button>Edit name</button>
+
+        email_address
+        <input type="text" name="email" value={}></input>
+
+        change Password
+        <Link to=""></Link>
+
+        signout
+        <Link to=""></Link>
+        </div>
+
     );
 };
 
