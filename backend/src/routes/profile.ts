ea37d593472c2acfec184d3f139fb5cd34871f57
kaushani rasadika <rasadikak@gmail.com>
2026-06-26 11:55:09 +0530

    profilepage.ts

--- backend/src/routes/profile.ts
+++ backend/src/routes/profile.ts
@@ -5,8 +5,37 @@ const profileRouter= Router();
 
 
 
+//get farmers name, district, email
+profileRouter.post("/get-farmer-info", authMiddleware,async(req:AuthRequest, res:Response)=>{
 
-profileRouter.post("/", authMiddleware,async(req:AuthRequest, res:Response)=>{
+    try{
+
+    }catch(error:any){
+        console.error("Get profile failed:", error);
+        res.status(500).json({error:"Failed to get profile"});
+    }
+
+});
+
+//change name
+profileRouter.put("/update-name", async(req:AuthRequest, res:Response)=>{
+    try{
+
+    }catch(error:any){
+        console.error("can not updatename:", error);
+        res.status(500).json({error:"Failed to edit name"});
+    }
+
+});
+
+//change district
+profileRouter.put("/update-district", async(req:AuthRequest, res:Response)=>{
+    try{
+
+    }catch(error:any){
+        console.error("can not update district:", error);
+        res.status(500).json({error:"Failed to edit district"});
+    }
 
 });
 
