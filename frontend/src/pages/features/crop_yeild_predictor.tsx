5afbd992ae25dcece82c4abf27024697db379daf
kaushani rasadika <rasadikak@gmail.com>
2026-07-07 14:38:08 +0530

    crop yield predictor.tsx

--- frontend/src/pages/features/crop_yeild_predictor.tsx
+++ frontend/src/pages/features/crop_yeild_predictor.tsx
@@ -1,7 +1,47 @@
+import  {useState} from "react"
+
 const CropYieldPredictorPage=()=>{
+
+    const CROPS= ['Cassava', 'Maize', 'Plantains and others', 'Potatoes',
+         'Rice, paddy', 'Sorghum', 'Soybeans', 'Sweet potatoes']
+
+    const [crop, setCrop]= useState("");
+
     return (
         <div>
             <h2>crop yeild predictor</h2>
+
+            <form>
+
+                <div>
+                    <label>crop</label>
+                    <select 
+                        value={crop} 
+                        onChange={(e) => setCrop(e.target.value)}
+                    >
+                    <option value="">Select a crop</option>
+                        {CROPS.map((c) => (
+                        <option key={c} value={c}>{c}</option>
+                        ))}
+                    </select>
+                </div>
+
+                <div>
+                    <label>year</label>
+                    <input type="number"></input>
+                </div>
+
+                <div>
+                    <label>temperature</label>
+                    <input type="number"></input>
+                </div>
+
+                <div>
+                    <label>pesticides_tonnes</label>
+                    <input type="number"></input>
+                </div>
+
+            </form>
         </div>
     )
 
