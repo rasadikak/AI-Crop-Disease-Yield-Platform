1fa7982dc94fde400eb729966614027bcf9860f3
kaushani rasadika <rasadikak@gmail.com>
2026-07-07 15:08:21 +0530

    crop yield predictor.tsx

--- frontend/src/pages/features/crop_yeild_predictor.tsx
+++ frontend/src/pages/features/crop_yeild_predictor.tsx
@@ -6,12 +6,40 @@ const CropYieldPredictorPage=()=>{
          'Rice, paddy', 'Sorghum', 'Soybeans', 'Sweet potatoes']
 
     const [crop, setCrop]= useState("");
+    const [year, setYear]= useState("");
+    const [temp, setTemp]= useState("");
+    const [pesticides, setPesticides]= useState("");
+
+    const [isLoading, setIsLoading]= useState(false);
+    const [error, setError]= useState("");
+
+    const handleSubmit= async(e:React.FormEvent)=>{
+        e.preventDefault();
+
+        if(!crop || !year || !temp || !pesticides){
+            setError("All fields are required");
+            return;
+        }
+
+        setIsLoading(true);
+        setError("");
+
+        try{
+
+        }catch(error:any){
+            setError(error.response?.data?.error || "signup failed");
+            return;
+        }finally{
+            setIsLoading(false);
+        }
+    }
+
 
     return (
         <div>
             <h2>crop yeild predictor</h2>
 
-            <form>
+            <form onSubmit={handleSubmit}>
 
                 <div>
                     <label>crop</label>
@@ -28,20 +56,30 @@ const CropYieldPredictorPage=()=>{
 
                 <div>
                     <label>year</label>
-                    <input type="number"></input>
+                    <input type="number" value={year} placeholder="2026" onChange={(e)=>{setYear(e.target.value)}}></input>
                 </div>
 
                 <div>
-                    <label>temperature</label>
-                    <input type="number"></input>
+                    <label>temperature(C)</label>
+                    <input type="number" placeholder="27" value={temp} onChange={(e)=>{setTemp(e.target.value)}}></input>
                 </div>
 
                 <div>
                     <label>pesticides_tonnes</label>
-                    <input type="number"></input>
+                    <input type="number" value={pesticides} placeholder="100" onChange={(e)=>{setPesticides(e.target.value)}}></input>
+                </div>
+
+                <div>
+                    <button type="submit" disabled={isLoading}>
+                        {isLoading ? "sending request":"submit"}
+                    </button>
                 </div>
 
             </form>
+
+            <div className="result">
+
+            </div>
         </div>
     )
 
