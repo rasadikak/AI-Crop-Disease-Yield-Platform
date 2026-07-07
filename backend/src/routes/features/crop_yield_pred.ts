678b3874ea82a6d3d20a7b1eee2ab50a8da45300
kaushani rasadika <rasadikak@gmail.com>
2026-07-07 11:58:41 +0530

    crop_yield_pred.ts

--- backend/src/routes/features/crop_yield_pred.ts
+++ backend/src/routes/features/crop_yield_pred.ts
@@ -8,6 +8,19 @@ crop_pred_router.post("/", authMiddleware,async(req:AuthRequest, res:Response)=>
     const {year}= req.body;
     const {temp}= req.body;
     const {pesticides}= req.body;
+
+    if (!crop || !year || !temp || !pesticides){
+        res.status(400).json({error:"all field are required"});
+    };
+
+    try{
+
+        
+
+    }catch(error:any){
+        console.error("crop yield pred  error", error);
+        res.status(500).json({error:"Failed to process"});
+    }
 });
 
 export default crop_pred_router;
