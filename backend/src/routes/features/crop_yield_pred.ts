137747dd918de0e1e6973227e6bf7add4d6f6306
kaushani rasadika <rasadikak@gmail.com>
2026-07-07 11:54:34 +0530

    crop_yield_pred.ts

--- backend/src/routes/features/crop_yield_pred.ts
+++ backend/src/routes/features/crop_yield_pred.ts
@@ -1,7 +1,13 @@
-import {Router} from "express";
+import {Router, Response} from "express";
+import authMiddleware, {AuthRequest} from "../../middleware/auth";
 
 const crop_pred_router= Router();
 
-
+crop_pred_router.post("/", authMiddleware,async(req:AuthRequest, res:Response)=>{
+    const {crop}= req.body;
+    const {year}= req.body;
+    const {temp}= req.body;
+    const {pesticides}= req.body;
+});
 
 export default crop_pred_router;
