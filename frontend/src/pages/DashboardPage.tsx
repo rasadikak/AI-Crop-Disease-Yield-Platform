04738f897b53ed36ddf78e9d16ac47d6f16def9d
kaushani rasadika <rasadikak@gmail.com>
2026-07-08 20:18:53 +0530

    added chatbot to dashboard page

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,21 +1,27 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import ChatbotPage from "./features/chatbot"; 
 
-const DashboardPage=()=>{
-    return(
-        <div>
-            <h2>dashboard</h2>
-            <Link to="/chatbot">chatbot</Link>
-            <br></br>
-            <Link to="/profile">profile</Link>
-            <br></br>
-            <Link to="/CropYeildPredictor">Crop Yeild Predictor</Link>
-            <br></br>
-            <Link to="/PlantDiseaseClassifier">Plant Disease Classifier</Link>
-            <br></br>
-            <Link to="/WeatherAnomalyDetector">Weather Anomaly Detector</Link>
+const DashboardPage = () => {
+    return (
+        <div style={{ display: "flex" }}>
+            {/* main dashboard content */}
+            <div style={{ flex: 1 }}>
+                <h2>dashboard</h2>
+                
+                <Link to="/profile">profile</Link>
+                <br />
+                <Link to="/CropYeildPredictor">Crop Yeild Predictor</Link>
+                <br />
+                <Link to="/PlantDiseaseClassifier">Plant Disease Classifier</Link>
+                <br />
+                <Link to="/WeatherAnomalyDetector">Weather Anomaly Detector</Link>
+            </div>
+
+            {/* chatbot floating on the right */}
+            <div style={{ position: "fixed", bottom: 20, right: 20, width: 320 }}>
+                <ChatbotPage />
+            </div>
         </div>
-
-
     );
 };
 
