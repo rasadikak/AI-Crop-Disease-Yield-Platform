62aa0dadba420437534e84084c586c0880a523b0
kaushani rasadika <rasadikak@gmail.com>
2026-07-09 16:30:04 +0530

    added css to ayth pages

--- frontend/src/pages/auth/ForgotPasswordPage.tsx
+++ frontend/src/pages/auth/ForgotPasswordPage.tsx
@@ -1,4 +1,4 @@
-// ─── ForgotPasswordPage.tsx ──────────────────────────────────────────────────
+
 import { useState } from "react";
 import { forgotPassword } from "../../services/authService";
 import { Link } from "react-router-dom";
