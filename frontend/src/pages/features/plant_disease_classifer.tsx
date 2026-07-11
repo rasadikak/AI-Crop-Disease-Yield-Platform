25269f3fd6585529cfc608489184182e1cadc9cf
kaushani rasadika <rasadikak@gmail.com>
2026-07-11 21:28:51 +0530

    plant_disease_classifier.tsx structurre

--- frontend/src/pages/features/plant_disease_classifer.tsx
+++ frontend/src/pages/features/plant_disease_classifer.tsx
@@ -2,6 +2,16 @@ const PlantDiseaseClassifierPage=()=>{
     return (
         <div>
             <h2>PlantDiseaseClassifierPage</h2>
+            <form>
+                <label>upload a clear picture of a leef of the plant</label>
+                <input type="file" />
+                <button type="submit">upload</button>
+                <div className="result-disease">
+                    
+
+                </div>
+                
+            </form>
         </div>
     )
 
