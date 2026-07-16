9a6d00318c71184b40453697f50aa49b5fa9277f
kaushani rasadika <rasadikak@gmail.com>
2026-07-16 18:58:55 +0530

    updated weather_anomaly.py

---
+++ backend/src/routes/features/weather_anomaly.ts
@@ -0,0 +1,9 @@
+import {Router, Request, Response} from "express";
+import authMiddleware, {AuthRequest} from "../../middleware/auth";
+
+const weather_anomaly_router= Router();
+
+weather_anomaly_router.post("/",authMiddleware, async(req:AuthRequest, res:Response)=>{
+    const {district}= req.body;
+    
+})
