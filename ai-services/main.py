9201453b753a8f3f7888862f6db4033848307f3b
kaushani rasadika <rasadikak@gmail.com>
2026-07-16 18:12:31 +0530

    updated app.py

--- ai-services/main.py
+++ ai-services/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import crop_yeild_predictor, chatbot
+from routers import crop_yeild_predictor, chatbot, weather_anomaly_detector
 
 app= FastAPI()
 
@@ -7,3 +7,4 @@ app= FastAPI()
 
 app.include_router(crop_yeild_predictor.router)
 app.include_router(chatbot.router)
+app.include_router(weather_anomaly_detector.router)
