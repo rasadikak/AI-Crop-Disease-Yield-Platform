908ee7ac0899ef9c1e89e223c4c6d11435314012
kaushani rasadika <rasadikak@gmail.com>
2026-07-17 10:34:06 +0530

    updated app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,7 @@ import crop_pred_router from "./routes/features/crop_yield_pred";
 import disease_classifier_router from "./routes/features/disease_classifier";
 import treatment_recommend_router from "./routes/features/treatment_recommender";
 import weather_anomaly_router from "./routes/features/weather_anomaly";
+import treatment_recommender_router from "./routes/features/treatment_recommender";
 
 const app= express();
 
@@ -28,6 +29,7 @@ app.use("/api/crop_yield_predictor", crop_pred_router);
 app.use("/api/disease_classifier", disease_classifier_router);
 app.use("/api/treatment_recommend", treatment_recommend_router);
 app.use("/api/weather_anomaly", weather_anomaly_router)
+app.use("/api/treatment", treatment_recommender_router)
 
 app.get("/health", (req,res)=>{
     res.json({status: "ok", message:"AgriSense API running"});
@@ -57,6 +59,7 @@ app.get("/health", (req,res)=>{
 //post http://localhost:3000/api/disease_classifier
 //post http://localhost:3000/api/treatment_recommend
 //post http://localhost:3000/api/weather_anomaly
+//post http://localhost:3000/api/treatment
 
 
 
